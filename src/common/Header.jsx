e63031eeeae6b3d1dc7717e6153de60a9f110682
BimlaJain <vj13798@gmail.com>
2024-12-20 11:29:29 +0530

    change dropdown

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -1,106 +1,78 @@
-import React, { useState } from "react";
-import navLogo from "../assets/png/logo.png";
-import rightArrow from "../assets/svg/right-arrow.svg";
-import Search from "../assets/svg/search.svg"
+import React, { useState, useEffect } from 'react'
+import logo from '../assets/png/logo.png'
+import { NAV_LIST } from '../utils/helper'
+import RightArrow  from '../assets/svg/right-arrow.svg'
+import  SearchIcon  from '../assets/svg/search.svg'
 
-const Header = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-    const toggleDropdown = (event) => {
-        const dropdown = event.currentTarget.nextElementSibling;
-        dropdown.classList.toggle("hidden");
-    };
-
+const Header = ({ myClass }) => {
+    const [open, setOpen] = useState()
+    useEffect(() => {
+        const handleOverflow = () => {
+            if (open && window.innerWidth < 770) {
+                document.body.classList.add("overflow-hidden");
+            } else {
+                document.body.classList.remove("overflow-hidden");
+            }
+        };
+        handleOverflow();
+        window.addEventListener("resize", handleOverflow);
+        return () => {
+            window.removeEventListener("resize", handleOverflow);
+        };
+    }, [open]);
     return (
-        <header>
-            <nav className="h-[80px] 2xl:container">
-                <div className="flex justify-between items-center max-lg:px-4 md:pl-14  lg:-mt-5">
-                    <img
-                        src={navLogo}
-                        alt="venveo-logo-img"
-                        className="w-[59px] h-[90px] mt-7"
-                    />
-                    <button
-                        className="flex flex-col gap-3 cursor-pointer lg:hidden relative z-20 transition-all max-lg:pt-6 duration-700 ease-in-out"
-                        onClick={() => setIsMenuOpen(!isMenuOpen)}
-                    >
-                        <span
-                            className={`w-9 h-1 rounded-full bg-white transition-transform ${isMenuOpen ? "rotate-45 translate-y-2" : ""
-                                }`}
-                        ></span>
-                       
-                        <span
-                            className={`w-9 h-1 rounded-full bg-white transition-transform ${isMenuOpen ? "-rotate-45 -translate-y-2" : ""
-                                }`}
-                        ></span>
-                    </button>
-                    <div
-                        className={`${isMenuOpen ? "block" : "hidden"
-                            } absolute top-0 left-0 w-full bg-white max-lg:bg-blue transition-all duration-700 ease-in-out lg:flex  max-lg:fixed lg:static lg:w-auto  items-center max-lg:min-h-screen  z-10 lg:pl-10`}
-                    >
-                        <div className="flex flex-col lg:flex-row gap-[42px] py-6 md:py-0 pl-6 md:pl-0 items-center max-lg:justify-center max-lg:min-h-screen">
-                            {[
-                                {
-                                    title: "How We Help",
-                                    links: [
-                                        "Drive More Awareness",
-                                        "Get More Customers",
-                                        "Build Your Brand",
-                                    ],
-                                },
-                                {
-                                    title: "Who We Help",
-                                    links: [
-                                        "B2B",
-                                        "Dealers & Distributors",
-                                        "Manufacturing & Industrial",
-                                    ],
-                                },
-                                {
-                                    title: "Why Venveo",
-                                    links: ["Who We Are", "Results", "Our Approach"],
-                                },
-                                {
-                                    title: "Resources",
-                                    links: ["Blog", "Case Studies", "Webinars"],
-                                },
-                            ].map((menu, index) => (
-                                <div key={index} className="relative dropdown">
-                                    <a
-                                        href="#"
-                                        className="text-sm font-medium lg:text-black text-white hover:text-yellow-500"
-                                        onClick={toggleDropdown}
-                                    >
-                                        {menu.title} <span>▼</span>
-                                    </a>
-                                    <div className="absolute hidden dropdown-content bg-white shadow-lg py-4 px-6 mt-2 z-10">
-                                        {menu.links.map((item, idx) => (
-                                            <a
-                                                key={idx}
-                                                href="#"
-                                                className="block text-sm text-black  hover:text-yellow mb-2"
-                                            >
-                                                {item}
-                                            </a>
-                                        ))}
-                                    </div>
-                                </div>
-                            ))}
-                            <img src={Search} alt="search" className="lg:fill-black fill-white" />
-                            <div className=" bg-yellow flex lg:py-8 lg:px-[31px] py-3 px-6 h-[81px]">
-                                <a
-                                    href="#"
-                                    className="text-custom-xs font-medium leading-normal flex items-center justify-center">
-                                    Let’s Talk <span><img className="lg:mt-2 ml-1" src={rightArrow} alt="arrow" /></span>
-                                </a>
-                            </div>
-                        </div>
+        <div className='max-w-[1920px] mx-auto max-md:pt-6 max-sm:pt-4 flex max-md:items-center justify-between'>
+            <div className=' max-w-max'>
+                <a href="#logo" className='relative z-10 '>
+                    <img src={logo} alt="venveo-logo" className='h-[90px] max-md:h-14 max-sm:h-16 mt-[30px] ml-8' />
+                </a>
+            </div>
+            <div className={`flex items-center ${myClass} max-md:hidden -mt-8 `}>
+                <div className='bg-white relative z-10 py-7 max-lg:py-4 flex item-center gap-[42px] max-lg:gap-5 max-lg:px-6 pl-[41.79px] pr-[29.5px]'>
+                    {NAV_LIST.map((obj, i) => (
+                        <select key={i} name="venveo" id="myLink" className='cursor-pointer text-[15px] leading-[15px] font-maisonMedium outline-none '>
+                            <option value={obj.title}>{obj.title}</option>
+                            <option value={obj.OptionOne}>{obj.OptionOne}</option>
+                            <option value={obj.OptionTwo}>{obj.OptionTwo}</option>
+                            <option value={obj.OptionThree}>{obj.OptionThree}</option>
+                        </select>
+                    ))}
+                    <div>                        
+                        <a href="">
+                            <img src={SearchIcon} alt="search-icon" className='group-hover:fill-blue transition-all duration-300' stroke='group-hover:stroke-blue transition-all duration-300' />
+                        </a>
                     </div>
                 </div>
-            </nav>
-        </header>
-    );
-};
+                <button className='flex items-center max-lg:py-[20.5px] py-[32.5px] px-[31.63px] bg-yellow gap-[5px] text-[15px] font-maisonDemi leading-[15px] font-semibold transition-all duration-300 hover:bg-black hover:text-white right-arrow'>Let's Talk <img src={RightArrow} alt="right-arrow" /></button>
+            </div>
+            <div
+                className={`md:hidden z-30 cursor-pointer pr-7`}
+                onClick={() => setOpen(!open)}
+            >
+                <div className={`flex flex-col gap-3 items-center ${open ? "!gap-0 transition-all duration-300" : "transition-all duration-300"}`}>
+                    <div className={`h-[2px] bg-white w-8 transition-all duration-300 rounded-lg ${open ? "rotate-[50deg] origin-center" : ""} `}></div>
+                    <div className={`h-[2px] bg-white w-8 transition-all duration-300 rounded-lg ${open ? "-rotate-[53deg] origin-center" : ""} `}></div>
+                </div>
+            </div>
+            <div className={`w-full h-full bg-blue transition-all duration-500 left-0 md:-top-full z-20 fixed flex flex-col items-center justify-center items-center ${open ? "top-0 left-0" : "-top-full"
+                }`}>
+                <div className=' relative z-10 flex flex-col item-center justify-center text-center gap-7'>
+                    {NAV_LIST.map((obj, i) => (
+                        <select key={i} name="venveo" id="myLink" className='custom-select cursor-pointer text-[15px] text-white leading-[15px] font-maisonMedium outline-none max-md:mx-auto bg-transparent'>
+                            <option value={obj.title}>{obj.title}</option>
+                            <option value={obj.OptionOne}>{obj.OptionOne}</option>
+                            <option value={obj.OptionTwo}>{obj.OptionTwo}</option>
+                            <option value={obj.OptionThree}>{obj.OptionThree}</option>
+                        </select>
+                    ))}
+                    <a href="">
+                        <img src={SearchIcon} alt="search-icon" className='group-hover:fill-blue transition-all duration-300' stroke='group-hover:stroke-blue transition-all duration-300' />
+                    </a>
+                    <button className='flex items-center max-lg:py-[20.5px] py-[32.5px] px-[31.63px] bg-yellow gap-[5px] text-[15px] font-maisonDemi leading-[15px] font-semibold transition-all duration-300 hover:bg-black hover:text-white right-arrow'>Let's Talk <img src={RightArrow} alt="right-arrow" /></button>
+                </div>
+            </div>
+        </div>
+    )
+}
 
-export default Header;
-
+export default Header
---
+++ src/utils/helper.jsx
@@ -0,0 +1,26 @@
+export const NAV_LIST = [
+    {
+        title: "How We Help",
+        OptionOne: "Item 1",
+        OptionTwo: "Item 2",
+        OptionThree: "Item 3",
+    },
+    {
+        title: "Who We Help",
+        OptionOne: "Item 1",
+        OptionTwo: "Item 2",
+        OptionThree: "Item 3",
+    },
+    {
+        title: "Why Venveo",
+        OptionOne: "Item 1",
+        OptionTwo: "Item 2",
+        OptionThree: "Item 3",
+    },
+    {
+        title: "Resources",
+        OptionOne: "Item 1",
+        OptionTwo: "Item 2",
+        OptionThree: "Item 3",
+    },
+]
