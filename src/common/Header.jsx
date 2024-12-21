487be9fc1b0bc66e6409deab81140df229a76e87
BimlaJain <vj13798@gmail.com>
2024-12-21 09:52:29 +0530

    change spacing

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -21,13 +21,13 @@ const Header = ({ myClass }) => {
         };
     }, [open]);
     return (
-        <div className='max-w-[1920px] mx-auto max-md:pt-6 max-sm:pt-4 flex max-md:items-center justify-between'>
+        <div className='max-w-[1920px] mx-auto max-lg:pt-6 max-sm:pt-4 flex max-lg:items-center justify-between'>
             <div className=' max-w-max'>
                 <a href="#logo" className='relative z-10 '>
                     <img src={logo} alt="venveo-logo" className='h-[90px] max-md:h-14 max-sm:h-16 mt-[30px] ml-8' />
                 </a>
             </div>
-            <div className={`flex items-center ${myClass} max-md:hidden -mt-8 `}>
+            <div className={`flex items-center ${myClass} max-lg:hidden -mt-8 `}>
                 <div className='bg-white relative z-10 py-7 max-lg:py-4 flex item-center gap-[42px] max-lg:gap-5 max-lg:px-6 pl-[41.79px] pr-[29.5px]'>
                     {NAV_LIST.map((obj, i) => (
                         <select key={i} name="venveo" id="myLink" className='cursor-pointer text-[15px] leading-[15px] font-maisonMedium outline-none '>
@@ -46,19 +46,19 @@ const Header = ({ myClass }) => {
                 <button className='flex items-center max-lg:py-[20.5px] py-[32.5px] px-[31.63px] bg-yellow gap-[5px] text-[15px] font-maisonDemi leading-[15px] font-semibold transition-all duration-300 hover:bg-black hover:text-white right-arrow'>Let's Talk <img src={RightArrow} alt="right-arrow" /></button>
             </div>
             <div
-                className={`md:hidden z-30 cursor-pointer pr-7`}
+                className={`lg:hidden z-30 cursor-pointer pr-7`}
                 onClick={() => setOpen(!open)}
             >
-                <div className={`flex flex-col gap-3 items-center ${open ? "!gap-0 transition-all duration-300" : "transition-all duration-300"}`}>
+                <div className={`flex flex-col gap-3 items-center ${open ? "!gap-0 transition-all duration-500" : "transition-all duration-500"}`}>
                     <div className={`h-[2px] bg-white w-8 transition-all duration-300 rounded-lg ${open ? "rotate-[50deg] origin-center" : ""} `}></div>
                     <div className={`h-[2px] bg-white w-8 transition-all duration-300 rounded-lg ${open ? "-rotate-[53deg] origin-center" : ""} `}></div>
                 </div>
             </div>
-            <div className={`w-full h-full bg-blue transition-all duration-500 left-0 md:-top-full z-20 fixed flex flex-col items-center justify-center ${open ? "top-0 left-0" : "-top-full"
+            <div className={`w-full h-full bg-blue transition-all duration-500 left-0 lg:-top-full z-20 fixed flex flex-col items-center justify-center ${open ? "top-0 left-0" : "-top-full"
                 }`}>
-                <div className=' relative z-10 flex flex-col item-center justify-center text-center gap-7'>
+                <div className=' relative z-10 flex flex-col item-center justify-center items-center text-center gap-7'>
                     {NAV_LIST.map((obj, i) => (
-                        <select key={i} name="venveo" id="myLink" className='custom-select cursor-pointer text-[15px] text-white leading-[15px] font-maisonMedium outline-none max-md:mx-auto bg-transparent'>
+                        <select key={i} name="venveo" id="myLink" className='custom-select cursor-pointer text-[15px] text-white leading-[15px] font-maisonMedium outline-none max-lg:mx-auto bg-transparent'>
                             <option value={obj.title}>{obj.title}</option>
                             <option value={obj.OptionOne}>{obj.OptionOne}</option>
                             <option value={obj.OptionTwo}>{obj.OptionTwo}</option>
